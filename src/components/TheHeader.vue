b20cf37d2a33314613f77f117b0894fc9cd19024
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-20 19:28:37 +0100

    WIP: header component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,125 @@
 <template>
-    <section>
-        The Header
-    </section>
+  <header class="nav__container">
+    <div class="header">
+      <div class="header__logo">
+        <router-link to="/">
+          <img
+            src="https://img.icons8.com/color/36/000000/pikachu-pokemon.png"
+          />
+        </router-link>
+      </div>
+
+      <ul
+        class="header__menu"
+        :class="[dropdownOpen ? 'active' : '']"
+        role="navigation"
+        aria-label="Main"
+      >
+        <li class="header__menu-item" @click="dropdownOpen = false">
+          <router-link to="/pokemons">Pokemons</router-link>
+        </li>
+        <li class="header__menu-item" @click="dropdownOpen = false">
+          <router-link to="/poketypes">Poketypes</router-link>
+        </li>
+        <li class="header__menu-item" @click="dropdownOpen = false">
+          <router-link to="/favourites">Favourites</router-link>
+        </li>
+      </ul>
+
+      <nav class="header__menu-controller" role="navigation" aria-label="Main">
+        <div class="header__menu-icons">
+          <span
+            @click="
+              dropdownIcon === 'menu'
+                ? (dropdownOpen = true)
+                : (dropdownOpen = false)
+            "
+          >
+            <i :class="'mdi mdi-' + dropdownIcon"></i>
+          </span>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
+  name: 'TheHeader',
 
-}
+  data() {
+    return {
+      dropdownOpen: false,
+    };
+  },
+
+  computed: {
+    dropdownIcon() {
+      return this.dropdownOpen ? 'close' : 'menu';
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped lang="scss">
+.nav__container {
+  @include set-container;
+  font-family: $primary-font;
 
+  .header {
+    @include set-width(4rem);
+
+    @include respond(tab-port) {
+      padding: 0 2rem;
+    }
+
+    &__menu {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      @include respond(tab-port) {
+        position: fixed;
+        left: -100%;
+        top: 5rem;
+        padding: 4rem 0;
+        z-index: 1;
+        flex-direction: column;
+        background-color: $red;
+        color: $white;
+        width: 100%;
+        text-align: center;
+        transition: 0.3s;
+        box-shadow: 0 10px 27px rgba(0, 0, 0, 0.05);
+      }
+
+      &-item {
+        @include respond(tab-port) {
+          margin-top: 1rem;
+          // padding-top: 1rem;
+        }
+        a {
+          // font-weight: bold;
+          font-size: 1.4rem;
+          margin-right: 1rem;
+        }
+      }
+
+      &.active {
+        @include respond(tab-port) {
+          left: 0;
+        }
+      }
+    }
+
+    &__menu-controller {
+      display: none;
+
+      @include respond(tab-port) {
+        display: block;
+        cursor: pointer;
+      }
+    }
+  }
+}
 </style>
