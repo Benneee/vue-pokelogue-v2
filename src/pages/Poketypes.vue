5e69c60499e6cbc3fcfd8f9057409c961f91a69f
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-22 21:57:40 +0100

    Feat: data for poketypes page

--- src/pages/Poketypes.vue
+++ src/pages/Poketypes.vue
@@ -1,15 +1,48 @@
 <template>
   <section>
-      Poketypes
+    <ul>
+      <li v-for="(type, index) in pokemonTypes" :key="`type-${index}`">
+        {{ type.name }}
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
-export default {
+import { mapActions, mapGetters } from 'vuex';
 
-}
+export default {
+  name: 'Poketypes',
+
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+
+  computed: {
+    ...mapGetters(['pokemonTypes']),
+  },
+
+  created() {
+    this.getPokemonTypes();
+  },
+
+  methods: {
+    ...mapActions(['fetchPokemonTypes']),
+
+    async getPokemonTypes() {
+      this.isLoading = true;
+
+      try {
+        await this.fetchPokemonTypes();
+      } catch (error) {
+        console.error('error: ', error);
+      }
+      this.isLoading = false;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
