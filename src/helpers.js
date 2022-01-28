35e4cd19cbba855f03f1544793e36e614d69e62f
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-28 15:57:00 +0100

    Feat: converted helpers file to module

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,9 @@
-export default function getPokemonId(url) {
+function getPokemonId(url) {
   return Number(url.split('/')[6]);
 }
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+const imageUrl =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+export { getPokemonId, apiUrl, imageUrl };
