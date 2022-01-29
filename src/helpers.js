a0668216d845176d9f306f4acece07c6b47628dd
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-29 09:24:22 +0100

    Feat: added new method to build-up pokemon details

--- src/helpers.js
+++ src/helpers.js
@@ -6,4 +6,49 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 const imageUrl =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-export { getPokemonId, apiUrl, imageUrl };
+function buildUpPokemonDetails(data) {
+  if (data) {
+    const { name, base_experience, height, weight, types, abilities } = data;
+    const pokemonData = {
+      abilities,
+      base_experience,
+      height,
+      name,
+      types,
+      weight,
+    };
+    const pokemonImg = data.sprites.front_default;
+    let usefulStats = {};
+    let baseStats = [...data['stats']];
+    baseStats.map((stat) => {
+      switch (stat.stat.name) {
+        case 'hp':
+          usefulStats['hp'] = stat['base_stat'];
+          break;
+        case 'attack':
+          usefulStats['attack'] = stat['base_stat'];
+          break;
+        case 'defense':
+          usefulStats['defense'] = stat['base_stat'];
+          break;
+        case 'speed':
+          usefulStats['speed'] = stat['base_stat'];
+          break;
+        case 'special-attack':
+          usefulStats['specialAttack'] = stat['base_stat'];
+          break;
+        case 'special-defense':
+          usefulStats['specialDefense'] = stat['base_stat'];
+          break;
+      }
+    });
+
+    return {
+      usefulStats,
+      pokemonData,
+      pokemonImg,
+    };
+  }
+}
+
+export { getPokemonId, apiUrl, imageUrl, buildUpPokemonDetails };
