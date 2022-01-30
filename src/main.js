ea43af0472206b9588fb352f02ba76257d758df2
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-30 05:29:51 +0100

    Feat: added notification plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
-import router from './router'
-import store from './store'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
+import store from './store';
+import App from './App.vue';
+import Notifications from '@kyvg/vue3-notification';
 
-const app = createApp(App)
-app.use(router)
-app.use(store)
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.use(Notifications);
 
-app.mount('#app')
+app.mount('#app');
